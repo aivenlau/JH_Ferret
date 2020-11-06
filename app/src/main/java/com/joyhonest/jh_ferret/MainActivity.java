755bebb9103ff823d2a2b2c323ca5b1e84e823b2
小米哥 <>
2020-11-06 11:18:09 +0800

    no message

--- app/src/main/java/com/joyhonest/jh_ferret/MainActivity.java
+++ app/src/main/java/com/joyhonest/jh_ferret/MainActivity.java
@@ -10,11 +10,11 @@ import com.joyhonest.ferrent_new_android.SplashActivity;
 
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         findViewById(R.id.btn_Start).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
